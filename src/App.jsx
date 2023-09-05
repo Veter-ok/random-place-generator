e84c59f92bc9384c8d85a74df1943e45a6ffe099
Lavrov Rodion <61391385+Veter-ok@users.noreply.github.com>
2023-09-05 21:32:19 +0300

    (feature) add yandex map

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { YMaps, Map, Placemark} from '@pbe/react-yandex-maps';
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentlyCoord, setCurrentlyCoord] = useState([55.751574, 37.573856])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>My awesome application with maps!</h1>
+      <YMaps>
+          <Map 
+            width={1000} 
+            height={600} 
+            defaultState={{ center: [55.75, 37.57], zoom: 10 }}
+            >
+            <Placemark geometry={currentlyCoord} />
+          </Map>
+      </YMaps>
     </>
   )
 }
