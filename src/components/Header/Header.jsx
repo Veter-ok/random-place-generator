ad1b108ddb968824d3eb540c69c4cd5c5c471916
Lavrov Rodion <61391385+Veter-ok@users.noreply.github.com>
2023-09-29 21:52:41 +0300

    (feat) add hamburger

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,8 +9,19 @@ const Header = () => {
 			<ul className='links'>
 				<li className='link'><a href='https://github.com/Veter-ok'>GitHub</a></li>
 				<li className='link'><a>Instagram</a></li>
-				<li className='link'><a>Меню</a></li>
+				<li className='link'><a>Menu</a></li>
 			</ul>
+			<div className="hamburger-menu">
+				<input id="menu__toggle" type="checkbox" />
+				<label className="menu__btn" htmlFor="menu__toggle">
+				<span></span>
+				</label>
+				<ul className="menu__box">
+					<li><a className="menu__item" href="https://github.com/Veter-ok">GitHub</a></li>
+					<li><a className="menu__item" href="https://instagram.com/veter.ok77">Instagram</a></li>
+					<li><a className="menu__item" href="#">Menu</a></li>
+				</ul>
+			</div>
 		</header>
 	)
 }
